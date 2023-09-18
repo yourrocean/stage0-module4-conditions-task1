487e009d4674f235ed216db57bbdc5c4b4c2a121
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-18 15:27:17 +0600

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int a = 5;
+        if (a<10){
+            System.out.println(false);}
     }
+    
 }
